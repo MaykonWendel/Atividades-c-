111bb4f53590fd373089bf3c758c52d22e3fba59
Maykon Wendel <maykonwendel12@alu.ufc.br>
2024-05-26 16:57:40 -0300

    Update a.cpp

--- DouglasMaiolli - atvdd/a3/a.cpp
+++ DouglasMaiolli - atvdd/a3/a.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
+
+float potencia(float x, float y){
+    float pot = 1;
+    int cont = 1;
+    while(cont <= y){
+        pot *= x;
+        cont++;
+    }
+    return pot;
+}
 
 int main(){
 
     int e, op;
-    float b, res;
+    float b;
 
-    do{
-        cout << "\nDigite o número da base: ";
-        cin >> b;
+    while(1){
+    cout << "\nDigite o número da base: ";
+    cin >> b;
 
-        cout << "Digite o número do expoente: ";
-        cin >> e;
+    cout << "Digite o número do expoente: ";
+    cin >> e;
 
-        res = pow(b,e);
-
-        cout << "O resultado da potência é:" << res << endl;
-
-        cout << "\nDeseja fazer outra potênciação?\n Não= 0\n Sim = 1" << endl;
-        cin >> op;
-
-        
-
-    }while(op == 1);
+    float respot = potencia(b,e);
     
+    cout << "O resultado da potência é:" << respot << endl;
 
+    cout << "\nDeseja fazer outra potênciação?\n Não= 0\n Sim = 1" << endl;
+    cin >> op;
+    if(op == 0){
+        break;
+    }
+
+    }
+    
     return 0;
 }
